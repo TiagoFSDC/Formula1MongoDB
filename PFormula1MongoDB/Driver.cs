b7acd3b09022004340de3b3170e47b3d1f454ea4
TiagoFSDC <tiagofsdc@gmail.com>
2023-04-13 08:32:53 -0300

    Add project files.

---
+++ PFormula1MongoDB/Driver.cs
@@ -0,0 +1,51 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PFormula1MongoDB
+{
+    internal class Driver
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] // Notações de tipos de dados(Data notation)
+        public string? Id { get; set; }
+
+        [BsonElement("Driver")] // Serealização
+        public string? Name { get; set; }
+
+        [BsonElement("Abbreviation")]
+        public string? Abbreviation { get; set; }
+
+        [BsonElement("No")]
+        [BsonRepresentation(BsonType.Int32)]
+        public int Number { get; set; }
+
+        [BsonElement("Team")]
+        public string? Team { get; set; }
+
+        [BsonElement("Country")]
+        public string? Country { get; set; }
+
+        [BsonElement("Date of Birth")]
+        public string? BirthDate { get; set; }
+
+        [BsonElement("Numero de vitorias")]
+        public int Numvitorias { get; set; }
+
+        public Driver(string? name, string? abbreviation, int number, string? team, string? country, string? birthDate,int numvitorias)
+        {
+            Name = name;
+            Abbreviation = abbreviation;
+            Number = number;
+            Team = team;
+            Country = country;
+            BirthDate = birthDate;
+            Numvitorias = numvitorias;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome : {Name} \nAbreviação : {Abbreviation} \nNumero do carro: {Number}" +
+                $"\nEquipe: {Team}\nPaís de Origem: {Country}\nData de nascimento : {BirthDate}\n";
+        }
+    }
+}
---
+++ PFormula1MongoDB/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Net.WebSockets;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using PFormula1MongoDB;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MongoClient mongo = new MongoClient("mongodb://localhost:27017");
+
+        var basedados = mongo.GetDatabase("Formula1");
+
+        var collection = basedados.GetCollection<BsonDocument>("Pilotos");
+
+        //Console.WriteLine("Informe o nome do piloto: ");
+        //string n = Console.ReadLine();
+
+        //var filtro = Builders<BsonDocument>.Filter.Regex("Driver", n);
+
+        //var p = collection.Find(filtro).FirstOrDefault();
+
+        //var piloto = BsonSerializer.Deserialize<Driver>(p); // Converte o arquivo Bson para Driver
+
+        //Console.WriteLine("\n" + piloto.ToString());
+
+        //Console.WriteLine("Nome: ");
+        //string n = Console.ReadLine();
+
+        //Console.WriteLine("Abreviação: ");
+        //string a = Console.ReadLine();
+
+        //Console.WriteLine("Numero: ");
+        //int num = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine("Equipe: ");
+        //string e = Console.ReadLine();
+
+        //Console.WriteLine("Pais: ");
+        //string p = Console.ReadLine();
+
+        //Console.WriteLine("Data de nascimento: ");
+        //string d = Console.ReadLine();
+
+        //Console.WriteLine("Numero de Vitorias: ");
+        //int vitorias = int.Parse(Console.ReadLine());
+
+        //Driver driver = new(n, a, num, e, p, d, vitorias);
+
+        //Console.WriteLine(driver.ToString());
+
+        //var dr = new BsonDocument
+        //{
+        //    {"Driver", driver.Name},
+        //    {"Abbreviation", driver.Abbreviation},
+        //    {"No", driver.Number},
+        //    {"Team", driver.Team},
+        //    {"Country", driver.Country},
+        //    {"Date of Birth", driver.BirthDate},
+        //    {"Numero de vitorias", driver.Numvitorias}
+        //};
+
+        //Console.WriteLine(dr);
+        //collection.InsertOne(dr);
+
+        Console.WriteLine("Informe o nome do piloto que sera modificado: ");
+        string n = Console.ReadLine();
+
+        var p = collection.Find(Builders<BsonDocument>.Filter.Regex("Driver", n)).First();
+
+        var driver = BsonSerializer.Deserialize<Driver>(p);
+
+        Console.WriteLine("Informe o novo numero: ");
+        int m = int.Parse(Console.ReadLine());
+
+        var filtro = Builders<BsonDocument>.Filter.Regex("Driver", n);
+        var update = Builders<BsonDocument>.Update.Set("No", m);
+
+        collection.UpdateOne(Builders<BsonDocument>.Filter.Regex("Driver", n),
+            Builders<BsonDocument>.Update.Set("No", m));
+
+        collection.DeleteOne(filtro);
+    }
+}
